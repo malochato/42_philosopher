6b66562b7bbcda547bc93b7e7ce31e395a3ae811
Malo <malo.dechateauvieux@gmail.com>
2025-03-12 23:54:53 +0800

    initialiaing finished_philo to 0

---
+++ philo_bonus/main.c
@@ -0,0 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malde-ch <malo@chato.fr>                   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/09 23:58:51 by malde-ch          #+#    #+#             */
+/*   Updated: 2025/03/12 23:50:04 by malde-ch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	free_all(t_config *config)
+{
+	int	i;
+
+	i = 0;
+	if (config->forks)
+	{
+		while (i < config->nb_philosophers)
+		{
+			pthread_mutex_destroy(&config->forks[i]);
+			i++;
+		}
+		free(config->forks);
+	}
+	i = 0;
+	if (config->philosophers)
+	{
+		while (i < config->nb_philosophers)
+		{
+			pthread_mutex_destroy(&config->philosophers[i]->finished_mutex);
+			free(config->philosophers[i]);
+			i++;
+		}
+		free(config->philosophers);
+	}
+	free(config);
+}
+
+int	start_the_feast(t_config *config)
+{
+	int	i;
+
+	i = 0;
+	while (i < config->nb_philosophers)
+	{
+		if (pthread_create(&config->philosophers[i]->thread, \
+		NULL, &routine, config->philosophers[i]) != 0)
+		{
+			ft_putstr_fd("Error: pthread_create failed\n", 2);
+			return (1);
+		}
+		i ++;
+	}
+	return (0);
+}
+
+int	join_philosophers(t_config *config)
+{
+	int	i;
+
+	i = 0;
+	while (i < config->nb_philosophers)
+	{
+		if (pthread_join(config->philosophers[i]->thread, NULL) != 0)
+		{
+			ft_putstr_fd("Error: pthread_join failed\n", 2);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+    t_config	*config;
+
+    printf("Allocating memory for config\n");
+    config = malloc(sizeof(t_config));
+    if (!config)
+        return (ft_putstr_fd("Error: malloc failed\n", 2), 1);
+    printf("Initializing config\n");
+    if (init_config(argc, argv, config))
+        return (free_all(config), 1);
+    printf("Starting the feast\n");
+    if (start_the_feast(config))
+        return (free_all(config), 1);
+    printf("Monitoring\n");
+    monitor(config);
+    printf("Joining philosophers\n");
+    if (join_philosophers(config))
+        return (free_all(config), 1);
+    printf("Freeing all resources\n");
+    free_all(config);
+    printf("Exiting program\n");
+    return (0);
+}
+
+
+/* int	main(int argc, char **argv)
+{
+	t_config	*config;
+
+	config = malloc(sizeof(t_config));
+	if (!config)
+		return (ft_putstr_fd("Error: malloc failed\n", 2), 1);
+	if (init_config(argc, argv, config))
+		return (free_all(config), 1);
+	if (start_the_feast(config))
+		return (free_all(config), 1);
+	monitor(config);
+	if (join_philosophers(config))
+		return (free_all(config), 1);
+	free_all(config);
+	return (0);
+} */
+
+/* 
+The return value take only one value, 
+but with the comma, the expression is still evaluated,
+in our case free_all(config) is evaluated before the return.
+ */
---
+++ philo_bonus/philo.h
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malde-ch <malo@chato.fr>                   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 15:34:19 by malde-ch          #+#    #+#             */
+/*   Updated: 2025/03/11 01:27:41 by malde-ch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <limits.h>
+
+# define PHILO_MAX 200
+
+typedef struct s_philosopher	t_philosopher;
+
+typedef enum e_philo_state
+{
+	thinking,
+	eating,
+	sleeping,
+	finished
+}	t_philo_state;
+
+typedef struct s_config
+{
+	int				nb_philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				number_of_times_each_philosopher_must_eat;
+
+	t_philosopher	**philosophers;
+	long long		start_time;
+	int				philo_dead;
+
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	print_mutex;
+
+}	t_config;
+
+typedef struct s_philosopher
+{
+	int				id;
+	t_philo_state	current_state;
+	long long		last_meal_time;
+	t_config		*config;
+	pthread_t		thread;
+
+	pthread_mutex_t	*print_mutex;
+	int				nb_time_eaten;
+
+	int				finished;
+	pthread_mutex_t	finished_mutex;
+
+}	t_philosopher;
+
+//utils.c
+void		ft_putstr_fd(char *str, int fd);
+void		ft_usleep(long long time);
+long long	get_current_time(void);
+
+//config.c
+int			init_config(int argc, char **argv, t_config *config);
+
+//parser.c
+int			parser(int argc, char **argv, t_config *config);
+
+//monitor.c
+int			monitor(t_config *config);
+
+//routine.c
+void		*routine(void *arg);
+void		need_to_talk(t_philosopher *philosopher, char *str);
+
+#endif
